3ecce1d5693bed37b17ca83b91a7de0c1e533708
Yatoorooo <Egvarakcin@gmail.com>
2024-01-13 12:41:00 +0300

    3 commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// FIRST TASK=///////////////////
 const arr = [
   16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
   76, -4, 12, -35, 4, 47,
@@ -13,3 +13,43 @@ const arrPositiv = arr.reduce(function (acc, val) {
   return val > 0 ? acc + 1 : acc;
 }, 0);
 console.log(arrPositiv);
+
+// two task//////////////////////////////////////
+let minIndex = 0;
+let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+     minIndex = i;
+    }
+  }
+console.log(`Мінімальне значення : ${min} та його індекс ${minIndex}`);
+
+let maxIndex = 0;
+let max = arr[0];
+for(let i = 0 ; i < arr.length; i++){
+  if(arr[i] > max){
+    max = arr[i];
+    maxIndex = i;
+  }
+}
+console.log(`Максимальне значення: ${max} та його індекс ${maxIndex}`);
+// TASK 4////////////////////////////////////////////////////
+function negative(elem, index, arr){
+  return !isNaN(elem) && elem < 0 ;
+} 
+ let negArr = arr.filter(negative);
+ console.log(negArr);
+//  ////////////////////////////////////////////////////
+
+function double(elem,  arr){
+  for (let i = 1; i < arr; i++){
+    if(elem % 2 !== 0 && elem > 0){
+      return elem;
+    }
+  }
+};
+
+let noDouble = arr.filter(double);
+console.log(noDouble);
